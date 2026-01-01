3498722b9ef1de45be5a42ce9c2357473dd7c9de
Yogasastha K <yogasasthak@gmail.com>
2026-01-01 23:15:01 +0530

    adding the data.json and try to use tanstack table for display leaderboard

--- WT-frontend/src/Components/Leaderboard.jsx
+++ WT-frontend/src/Components/Leaderboard.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import data from '@/data/leaderboard.json';
+// D:\Project\React\Workout Tracker\WT-frontend\src\data\leaderboard.json
 
+const {profile, stats} = data; 
 const Leaderboard = () => {
   return (
     <div>
-      Leaderboard
     </div>
   )
 }
